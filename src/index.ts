be3214968fec0e3c3a7142ad3afa07089e2fc3ab
maxfer03 <sandsferdinand@gmail.com>
2021-09-10 22:49:50 -0300

    updated package.json and gitignore

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express()
+const port: number = 3001
+
+
+
+app.set('port', port)
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`)
+})
