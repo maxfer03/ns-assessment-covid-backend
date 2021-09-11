795445d636010200f1515dc03268abd041ce092e
maxfer03 <sandsferdinand@gmail.com>
2021-09-10 22:51:21 -0300

    initialized server

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,5 @@ const port: number = 3001
 app.set('port', port)
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+    console.log(`Server running on port ${port}.`)
 })
