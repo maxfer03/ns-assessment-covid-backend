ffc2e7b615b5d1edb5d219a94a3664aaf1082c26
maxfer03 <sandsferdinand@gmail.com>
2021-09-11 11:58:06 -0300

    added express types

--- src/routes/auth/auth.ts
+++ src/routes/auth/auth.ts
@@ -1,19 +1,26 @@
-import {Router} from "express";
-import User from '../../models/user'
+import { Router, Request, Response } from "express";
+import { Iuser } from "../../utils/interfaces";
+import User from "../../models/user";
 
-const auth: Router = Router()
+const auth: Router = Router();
 
-auth.get('/', (req, res) => {
-    return res.send('auth path')
-})
+auth.get("/", (req: Request, res: Response) => {
+  return res.send("auth path");
+});
 
-auth.post('/login', (req, res) => {
-    return res.json('log in')
-})
+auth.post("/login", (req: Request, res: Response) => {
+  return res.json("log in");
+});
 
-auth.post('/signup', (req, res) => {
-    return res.json('sign up')
-})
+auth.post("/signup", (req: Request, res: Response) => {
+  const user: Iuser = req.body;
+  console.log(user);
+  if (!user) {
+    return res.status(400).send("ERROR: Empty user.");
+  } else {
+    
+    return res.json(`User ${user.username} created.`);
+  }
+});
 
-
-export default auth
+export default auth;
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,35 +1,29 @@
-import {Router} from "express";
+import { Router, Request, Response } from "express";
 import auth from "./auth/auth";
 import { requestCovidStats } from "../utils/axios";
 import { IcovidStats } from "../utils/interfaces";
 import User from "../models/user";
-const routes: Router = Router()
+const routes: Router = Router();
 
+routes.use("/auth", auth);
 
-routes.use('/auth', auth)
+routes.get("/statistics", async (req: Request, res: Response) => {
+  const info: IcovidStats | string = await requestCovidStats();
+  return res.json(info);
+  //return res.json('stats')
+});
 
+routes.post("/statistics", (req: Request, res: Response) => {
+  return res.json("stats posted");
+});
 
+routes.get("/sync", (req: Request, res: Response) => {
+  return res.json("sync");
+});
 
+routes.get("/users", async (req: Request, res: Response) => {
+  const users = await User.find();
+  return res.json(users);
+});
 
-routes.get('/statistics', async (req, res) => {
-    const info: IcovidStats | string = await requestCovidStats()
-    return res.json(info)
-    //return res.json('stats')
-})
-
-routes.post('/statistics', (req, res) => {
-    return res.json('stats posted')
-})
-
-routes.get('/sync', (req, res) => {
-    return res.json('sync')
-})
-
-routes.get('/users', async (req, res) => {
-    const users = await User.find()
-    return res.json(users)
-} )
-
-
-
-export default routes
+export default routes;
