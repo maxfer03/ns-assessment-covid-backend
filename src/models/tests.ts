e04cfdd49a452080484aa3371854ee28e05ddc8b
maxfer03 <sandsferdinand@gmail.com>
2021-09-11 20:20:20 -0300

    deleted unused schemas

--- src/models/tests.ts
+++
@@ -1,9 +0,0 @@
-import { Schema, model } from "mongoose";
-import { Icases, Ideaths, Itests } from "../utils/interfaces";
-
-const TestsSchema = new Schema({
-  "1M_pop":     {type: String},
-  total:        {type: Number}
-});
-
-export default model("Tests", TestsSchema);
