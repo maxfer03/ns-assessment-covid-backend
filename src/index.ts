2b1ece3517c705d061b0cd160de30977f3a6e3a8
maxfer03 <sandsferdinand@gmail.com>
2021-09-12 09:01:28 -0300

    configured cors

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,18 @@ app.set("port", process.env.PORT || port);
 //middlewares
 app.use(express.json());
 app.use(morgan("dev"));
-app.use(cors());
 app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  next();
+});
 app.use("/", routes);
 
 app.get("/", (req, res) => {
